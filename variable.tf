cab6db785e8dffc4a1df8e86c70b87106c9dc984
kuldeep <abc@kuldeeps-MacBook-Air.local>
2026-06-02 16:37:08 +0530

    first commit to git

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name"{
+    type = string
+    description = "This is for resource group name"
+    default = "terraform-rg"
+}
+
+variable "location" {
+    type = string
+    description = "This is for location variable"
+    default = "West Europe"
+}
